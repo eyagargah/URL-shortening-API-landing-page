e49bb6bf63acdab00d1a23ac5c2898791403d26f
Eya Gargah <eyagargah@hotmail.com>
2022-12-10 00:01:04 +0100

    create component link-section

---
+++ url-shortening-api-page/src/app/pages/link-section/link-section.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LinkSectionComponent } from './link-section.component';
+
+describe('LinkSectionComponent', () => {
+  let component: LinkSectionComponent;
+  let fixture: ComponentFixture<LinkSectionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LinkSectionComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LinkSectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ url-shortening-api-page/src/app/pages/link-section/link-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-link-section',
+  templateUrl: './link-section.component.html',
+  styleUrls: ['./link-section.component.scss']
+})
+export class LinkSectionComponent {
+
+}
