eff5d32efe0f630bd0bbce96ab8608c667627fb9
Eya Gargah <eyagargah@hotmail.com>
2022-12-10 00:01:32 +0100

    create component service description

---
+++ url-shortening-api-page/src/app/pages/service-description/service-description.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-service-description',
+  templateUrl: './service-description.component.html',
+  styleUrls: ['./service-description.component.scss']
+})
+export class ServiceDescriptionComponent {
+
+}
