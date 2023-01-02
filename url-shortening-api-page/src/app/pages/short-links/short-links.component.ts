07596dd514de48620561b5745a7c935a71bb9683
Eya Gargah <eyagargah@hotmail.com>
2023-01-02 19:30:57 +0100

    Update short-links.component.ts

--- url-shortening-api-page/src/app/pages/short-links/short-links.component.ts
+++ url-shortening-api-page/src/app/pages/short-links/short-links.component.ts
@@ -3,14 +3,24 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-short-links',
   templateUrl: './short-links.component.html',
-  styleUrls: ['./short-links.component.scss']
+  styleUrls: ['./short-links.component.scss'],
 })
 export class ShortLinksComponent {
+  @Input() linkData: any;
 
-  @Input() linkData : any;
-
-   
-
+ toggle(event: Event){
+  let elementId = event.target as HTMLElement;
+  console.log(elementId.parentElement?.firstChild?.textContent)
+  if(elementId.classList.contains('not-active')){
+    elementId.classList.remove('not-active');
+    elementId.classList.add('active')
+    elementId.innerText= 'Copied!'
+  }else {
+    elementId.classList.add('not-active')
+    elementId.classList.remove('active')
+    elementId.innerText = 'Copy'
+  }
+ }
   
 
 }
