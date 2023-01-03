74bd6ca8ea63a859a1f350b3e0eedde48aa803af
Eya Gargah <eyagargah@hotmail.com>
2023-01-03 19:01:39 +0100

    Update short-links.component.ts
    add copyLink Method so we can copy/paste selected link

--- url-shortening-api-page/src/app/pages/short-links/short-links.component.ts
+++ url-shortening-api-page/src/app/pages/short-links/short-links.component.ts
@@ -1,16 +1,25 @@
 import { Component, Input } from '@angular/core';
+import { Clipboard } from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-short-links',
   templateUrl: './short-links.component.html',
   styleUrls: ['./short-links.component.scss'],
 })
+
 export class ShortLinksComponent {
   @Input() linkData: any;
 
- toggle(event: Event){
+  constructor( private clipboard: Clipboard){}
+
+  copyText(textToCopy: any){
+    this.clipboard.copy(textToCopy);
+  }
+  copyLink(event: Event){
   let elementId = event.target as HTMLElement;
   console.log(elementId.parentElement?.firstChild?.textContent)
+  let textContent = elementId.parentElement?.firstChild?.textContent;
+  this.copyText(textContent)
   if(elementId.classList.contains('not-active')){
     elementId.classList.remove('not-active');
     elementId.classList.add('active')
@@ -21,6 +30,6 @@ export class ShortLinksComponent {
     elementId.innerText = 'Copy'
   }
  }
-  
+
 
 }
