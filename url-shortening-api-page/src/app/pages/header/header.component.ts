be61a065e68374229bff4e90cbb2ab9995ef2148
Eya Gargah <eyagargah@hotmail.com>
2023-01-20 23:31:57 +0100

    Update header.component.ts

--- url-shortening-api-page/src/app/pages/header/header.component.ts
+++ url-shortening-api-page/src/app/pages/header/header.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  openMenu(){
+    let burger = document.querySelector('.burger')
+    let links = document.querySelector('.links')
+    if(links?.classList.contains("open")){
+      links.classList.remove('open')
+      links.classList.add('close')
+    }else {
+      links?.classList.add('open')
+      links?.classList.remove('close')
+    }
+    console.log('clicked')
+  }
+
+  
+
 }
