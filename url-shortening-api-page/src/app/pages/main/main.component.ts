78390963b2dc02179ccdcde8e940c175b11a503e
Eya Gargah <eyagargah@hotmail.com>
2023-03-29 23:44:40 +0100

    Update main.component.ts

--- url-shortening-api-page/src/app/pages/main/main.component.ts
+++ url-shortening-api-page/src/app/pages/main/main.component.ts
@@ -6,13 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.scss'],
 })
 export class MainComponent {
-  storedLinks : any =[] 
-  ngOnInit(){
-    console.log(this.storedLinks)
-    const keys = Object.keys(localStorage)
-    for(let key in keys){
-      this.storedLinks.push([keys[key] , localStorage.getItem(keys[key])])
-    }
-    console.log(this.storedLinks[0][0])
-  }
 }
