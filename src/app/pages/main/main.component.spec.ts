1e69b8c5590ba4e8f59e28cbf8172185f9ba12eb
Eya Gargah <eyagargah@hotmail.com>
2024-01-09 23:43:15 +0100

    Update main.component.spec.ts

--- src/app/pages/main/main.component.spec.ts
+++ src/app/pages/main/main.component.spec.ts
@@ -3,6 +3,11 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MainComponent } from './main.component';
 import { HeaderComponent } from '../header/header.component';
 import { ServiceDescriptionComponent } from '../service-description/service-description.component';
+import { LinkSectionComponent } from '../link-section/link-section.component';
+import { StatsComponent } from '../stats/stats.component';
+import { BenefitSectionComponent } from '../benefit-section/benefit-section.component';
+import { LinkBoostComponent } from '../link-boost/link-boost.component';
+import { FooterComponent } from '../footer/footer.component';
 
 describe('MainComponent', () => {
   let component: MainComponent;
@@ -10,7 +15,7 @@ describe('MainComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MainComponent , HeaderComponent , ServiceDescriptionComponent ]
+      declarations: [ MainComponent , HeaderComponent , ServiceDescriptionComponent, LinkSectionComponent, StatsComponent, BenefitSectionComponent , LinkBoostComponent, FooterComponent ]
     })
     .compileComponents();
 
